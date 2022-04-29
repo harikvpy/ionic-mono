96f5a493e989816fd71658433eb6e487f249bad7
Hari Mahadevan <hari@smallpearl.com>
2022-04-29 10:24:43 +0800

    Replace ng app with ionic blank app

--- apps/app-one/src/app/app.component.spec.ts
+++ apps/app-one/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'app-one'`, () => {
+  it(`should have as title 'admin'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('app-one');
+    expect(app.title).toEqual('admin');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('app-one app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('admin app is running!');
   });
 });
